40d27c09169a4163c8739b8b6a54ba83a6b77817
varun chaaras <chaarasvarun@gmail.com>
2025-09-30 17:19:26 +0530

    camera

--- VarunChaaras/src/main.js
+++ VarunChaaras/src/main.js
@@ -1,3 +1,22 @@
 import * as THREE from 'three';
 
 const scene = new THREE.scene();
+
+const cubeGeometry = new THREE.BoxGeometry(1,1,1);
+const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"});
+
+const cubeMesh = new THREE.Mesh(
+  cubeGeometry,
+  cubeMaterial
+);
+
+scene.add(cubeMesh);
+
+
+const camera = new THREE.PerspectiveCamera(
+  75, 
+  window.innerWidth/window.innerHeight,
+  0.1,
+  30);
+
+camera.position.z = 5;
