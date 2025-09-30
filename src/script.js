87ea834958b58813cf1e8969cd0f20955be19488
varun chaaras <chaarasvarun@gmail.com>
2025-09-30 17:49:02 +0530

    starter

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
 import * as THREE from 'three';
 
+// initialize the scene
 const scene = new THREE.Scene()
 
+// add objects to the scene
 const cubeGeometry = new THREE.BoxGeometry(1,1,1)
 const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"})
 
@@ -11,6 +13,7 @@ const cubeMesh = new THREE.Mesh(
 )
 scene.add(cubeMesh)
 
+// initialize the camera
 const camera = new THREE.PerspectiveCamera(
   75, 
   window.innerWidth / window.innerHeight,
@@ -18,9 +21,10 @@ const camera = new THREE.PerspectiveCamera(
   30)
 camera.position.z = 5
 
+// initialize the renderer
 const canvas = document.querySelector('canvas.threejs')
 const renderer = new THREE.WebGLRenderer({
-  canvas : canvas
+  canvas: canvas
 })
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.render(scene, camera)
