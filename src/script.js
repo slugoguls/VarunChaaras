350c57f97809d522d4ed959d9012d9cb150af81c
varun chaaras <chaarasvarun@gmail.com>
2025-10-01 19:56:18 +0530

    materials

--- src/script.js
+++ src/script.js
@@ -10,15 +10,33 @@ const scene = new THREE.Scene();
 
 // initialize the geometry
 const geometry = new THREE.BoxGeometry(1, 1, 1);
+const planeGeometry = new THREE.PlaneGeometry(1, 1);
 
 // initialize the material
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
-});
+const material = new THREE.MeshBasicMaterial();
 
+material.color = new THREE.Color(0x00ff00)
+material.transparent = true;
+material.opacity = 1;
+material.side = 2
+material.fog = false
+
+const fog = new THREE.Fog(0xffffff, 1, 10)
+scene.fog = fog
+scene.background = new THREE.Color(0xffffff)
 // initialize the mesh
 const mesh = new THREE.Mesh(geometry, material);
+
+const mesh2 = new THREE.Mesh(geometry, material);
+mesh2.position.x = 1.5
+
+const plane = new THREE.Mesh(planeGeometry, material);
+plane.position.x = -1.5
+plane.rotateX(THREE.MathUtils.degToRad(90))
+
 scene.add(mesh);
+scene.add(mesh2);
+scene.add(plane);
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
