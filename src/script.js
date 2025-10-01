afd718584a6d0eafae9535a0d38fce4c78602a54
varun chaaras <chaarasvarun@gmail.com>
2025-10-01 19:33:40 +0530

    reroll

--- src/script.js
+++ src/script.js
@@ -1,26 +1,24 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { Pane } from "tweakpane";
+
+// initialize the pane
+const pane = new Pane();
 
 // initialize the scene
 const scene = new THREE.Scene();
 
-// add objects to the scene
-//const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+// initialize the geometry
+const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-const verticies = new Float32Array([0, 0, 0, 0, 2, 0, 2, 0, 0]);
-
-const bufferAttribute = new THREE.BufferAttribute(verticies, 3)
-
-const geometry = new THREE.BufferGeometry();
-geometry.setAttribute('position', bufferAttribute)
-
-const cubeMaterial = new THREE.MeshBasicMaterial({
-  color: "red",
-  wireframe: true,
+// initialize the material
+const material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
 });
-const cubeMesh = new THREE.Mesh(geometry, cubeMaterial);
 
-scene.add(cubeMesh);
+// initialize the mesh
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
@@ -43,7 +41,6 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 // instantiate the controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-controls.autoRotate = false;
 
 window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
