be8ab593218a103be4d47eab82f1f60e910b4e18
varun chaaras <chaarasvarun@gmail.com>
2025-10-02 11:53:28 +0530

    groups

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { degToRad } from "three/src/math/MathUtils.js";
 import { Pane } from "tweakpane";
 
 // initialize the pane
@@ -12,9 +13,14 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const torusKnotGeometry = new THREE.TorusKnotGeometry(0.5, 0.15, 100, 16);
 const planeGeometry = new THREE.PlaneGeometry(1, 1);
+const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 32);
 
 // initialize the material
 const material = new THREE.MeshBasicMaterial();
+
+
+const group = new THREE.Group();
 
 // initialize the mesh
 const cube = new THREE.Mesh(geometry, material);
@@ -25,16 +31,28 @@ knot.position.x = 1.5;
 const plane = new THREE.Mesh(planeGeometry, material);
 plane.position.x = -1.5;
 
+const sphere = new THREE.Mesh()
+sphere.geometry = sphereGeometry;
+sphere.material = material
+sphere.position.y = 1.5;
+
+const cylinder = new THREE.Mesh();
+cylinder.geometry = cylinderGeometry;
+cylinder.material = material;
+cylinder.position.y = -1.5;
+
+
+
 // add the mesh to the scene
-scene.add(cube);
-scene.add(knot);
-scene.add(plane);
+
+group.add(sphere, cylinder, cube, knot, plane);
+scene.add(group);
 
 // initialize the light
-const light = new THREE.AmbientLight(0xffffff, 0.4);
+const light = new THREE.AmbientLight(0xffffff, 0.1);
 scene.add(light);
 
-const pointLight = new THREE.PointLight(0xffffff, 1.2);
+const pointLight = new THREE.PointLight(0xff00ff, 1);
 pointLight.position.set(5, 5, 5);
 scene.add(pointLight);
 
@@ -68,6 +86,15 @@ window.addEventListener("resize", () => {
 
 // render the scene
 const renderloop = () => {
+
+  group.children.forEach((child) => {
+
+    if (child instanceof THREE.Mesh){
+        child.rotation.y += 0.01
+    }
+
+  })
+
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(renderloop);
