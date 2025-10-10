06998cc34c3f14468b8dfe1bcff511898614e623
varun chaaras <chaarasvarun@gmail.com>
2025-10-10 16:28:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ export default {
     root: 'src/',
     publicDir: '../assets/',
     base: './',
+    build: {
+        target: 'esnext', // Support top-level await
+        outDir: '../dist'
+    },
     server: {
         host: true, // Expose to network
         port: 5173  // Default Vite port
